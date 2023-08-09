e6dc4bb3f9e560721fa4e38407b55feb71de553b
minzzweb <minzzweb@gmail.com>
2023-08-09 22:09:38 +0900

    first commit

---
+++ src/main/java/com/nature/NatureCoinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatureCoinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatureCoinBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nature/NatureCoinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatureCoinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
